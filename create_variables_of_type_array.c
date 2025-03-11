e0c4b67b7c940b2cbb8d16c11711d32668d989b5
WizzOFF <maher_off@proton.me>
2025-03-11 19:19:32 +0100

    create_variables_of_type_array.c: initialize an array

--- create_variables_of_type_array.c
+++ create_variables_of_type_array.c
@@ -19,7 +19,7 @@ printf("%d", *tableau);
 tableau[1] // Renvoie la valeur de la seconde case (la première case étant 0)
 *(tableau + 1) // Identique : renvoie la valeur contenue dans la seconde case
 */
-
+/*
 int main(int argc, char *argv[])
 {
     int tableau[4], i = 0;
@@ -36,3 +36,37 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+*/
+
+int main(int argc, char *argv[])
+{
+    int tableau[4], i = 0;
+
+    // Initialisation du tableau
+    for (i = 0 ; i < 4 ; i++)
+    {
+        tableau[i] = 0;
+    }
+
+    // Affichage de ses valeurs pour vérifier
+    for (i = 0 ; i < 4 ; i++)
+    {
+        printf("%d\n", tableau[i]);
+    }
+
+    return 0;
+}
+
+/*
+int main(int argc, char *argv[])
+{
+    int tableau[4] = {0, 0, 0, 0}, i = 0;
+
+    for (i = 0 ; i < 4 ; i++)
+    {
+        printf("%d\n", tableau[i]);
+    }
+
+    return 0;
+}
+*/
