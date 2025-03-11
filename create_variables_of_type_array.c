93c1b863eb63c48d2ea6fdb3d211c9738d6b4820
WizzOFF <maher_off@proton.me>
2025-03-11 18:13:04 +0100

    create_variables_of_type_array.c: browse a table

--- create_variables_of_type_array.c
+++ create_variables_of_type_array.c
@@ -1,11 +1,11 @@
-
+/*
 int tableau[4];
 
 ableau[0] = 10;
 tableau[1] = 23;
 tableau[2] = 505;
 tableau[3] = 8;
-
+*/
 /*
 printf("%d", tableau);
 */
@@ -15,6 +15,24 @@ printf("%d", tableau[0]);
 /*
 printf("%d", *tableau);
 */
-
+/*
 tableau[1] // Renvoie la valeur de la seconde case (la première case étant 0)
 *(tableau + 1) // Identique : renvoie la valeur contenue dans la seconde case
+*/
+
+int main(int argc, char *argv[])
+{
+    int tableau[4], i = 0;
+
+    tableau[0] = 10;
+    tableau[1] = 23;
+    tableau[2] = 505;
+    tableau[3] = 8;
+
+    for (i = 0 ; i < 4 ; i++)
+    {
+        printf("%d\n", tableau[i]);
+    }
+
+    return 0;
+}
