2b91a11a7aaee415d8fdf02e7cfc492b2d654d0d
WizzOFF <maher_off@proton.me>
2025-03-11 19:30:14 +0100

    create_variables_of_type_array.c: function displays the contents of an array

--- create_variables_of_type_array.c
+++ create_variables_of_type_array.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 */
-
+/*
 int main(int argc, char *argv[])
 {
     int tableau[4], i = 0;
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
+*/
 /*
 int main(int argc, char *argv[])
 {
@@ -70,3 +70,25 @@ int main(int argc, char *argv[])
     return 0;
 }
 */
+
+void affiche(int *tableau, int tailleTableau);
+ 
+int main(int argc, char *argv[])
+{
+    int tableau[4] = {10, 15, 3};
+ 
+    // On affiche le contenu du tableau
+    affiche(tableau, 4);
+ 
+    return 0;
+}
+ 
+void affiche(int *tableau, int tailleTableau)
+{
+    int i;
+ 
+    for (i = 0 ; i < tailleTableau ; i++)
+    {
+        printf("%d\n", tableau[i]);
+    }
+}
